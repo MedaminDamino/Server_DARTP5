9afe0e8854319fd16fd422129ec0a6a18d120993
MedaminDamino <medamin.abdelkafi753@gmail.com>
2023-11-10 20:04:20 +0100

    TP5

---
+++ src/RMIService/IConversion.java
@@ -0,0 +1,9 @@
+package RMIService;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IConversion extends Remote {
+    public double convertirMontant (double mnt) throws RemoteException;
+
+}
